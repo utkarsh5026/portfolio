d07dd4c443c1aec5d32b2cc6ba7fd22278f29ae3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-12-05 19:08:31 +0530

    changed height of the rticle card

--- app/src/components/home/articles/ArticleCard.tsx
+++ app/src/components/home/articles/ArticleCard.tsx
@@ -13,7 +13,7 @@ const ArticleCard: React.FC<ArticleCardProps> = ({
   link,
 }) => {
   return (
-    <div className="bg-transparent rounded-lg transition-all duration-300 flex flex-col justify-between border-4 border-gray-500">
+    <div className="bg-transparent rounded-lg transition-all duration-300 flex flex-col justify-between border-4 border-gray-500 p-8 h-full">
       <div className="bg-slate-950 p-4 rounded-lg">
         <h2 className="text-lg font-normal text-left">{title}</h2>
         <p className="text-gray-400 text-sm my-6 text-left  ">{description}</p>
--- app/src/components/home/articles/Articles.tsx
+++ app/src/components/home/articles/Articles.tsx
@@ -8,8 +8,8 @@ const Articles: React.FC = () => {
     <Section id="articles" label="Articles">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
         {articles.map((article) => (
-          <div className="flex flex-col gap-4 h-64 m-2">
-            <ArticleCard key={article.title} {...article} />
+          <div className="h-full" key={article.title}>
+            <ArticleCard {...article} />
           </div>
         ))}
       </div>
