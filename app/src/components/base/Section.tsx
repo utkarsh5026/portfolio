a5c9cad2776c245918938eb1cbff21272ebd2df9
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-12-08 08:03:57 +0530

    changed the size of the heading

--- app/src/components/base/Section.tsx
+++ app/src/components/base/Section.tsx
@@ -17,7 +17,7 @@ const Section: React.FC<SectionProps> = ({
   return (
     <div id={id} className={`mt-64 ${className} relative`}>
       <div className="absolute -right-20 -top-20 w-1/2 h-96  rounded-full border-2 border-gray-500/10" />
-      <GradientText className="text-6xl font-bold text-right">
+      <GradientText className="text-8xl font-bold text-right">
         {label}
       </GradientText>
       <hr className="my-8" />
