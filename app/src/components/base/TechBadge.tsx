5130d95ad2d8bb91684efd5e18f037cdea035eb0
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-05 01:19:07 +0530

    Add documentation comment to TechBadge component, detailing its features and functionality

--- app/src/components/base/TechBadge.tsx
+++ app/src/components/base/TechBadge.tsx
@@ -1,3 +1,17 @@
+/**
+ * A clickable badge component that shows off different technologies! 🛠️
+ *
+ * This component creates those neat little tech pills you see throughout the app.
+ * Each badge shows an icon and the name of a technology, and when clicked,
+ * it'll take you to more info about that tech.
+ *
+ * Features:
+ * - Hover animations with a subtle glow effect
+ * - Click animation that scales the badge down
+ * - Dark mode support
+ * - Auto-fetches icons and links from our technologies config
+ */
+
 import React from "react";
 import { technologies } from "./technologies";
 
