28ca48c63cff12b0153819e74f0702b01955142f
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-11 04:40:49 +0530

    Add Vercel configuration and update Vite base path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Portfolio/",
+  base: "/",
   build: {
     sourcemap: process.env.NODE_ENV === "development",
     chunkSizeWarningLimit: 500,
