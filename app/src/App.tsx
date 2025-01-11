36ab713cf8b10ef2573eb214f0dfd4d42d630e24
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-11 06:26:40 +0530

    Refactor App component and remove game components
    - Removed the PingPong and MemoryGame components from the App, streamlining the codebase.
    - Updated the App layout to eliminate references to the removed game sections, enhancing overall organization and maintainability.
    - Adjusted imports and lazy loading to reflect the changes, ensuring a cleaner structure for the application.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,8 +7,7 @@ import NavigationBar from "./components/home/appbar/NavigationBar";
 const Skills = lazy(() => import("./components/home/skills/Skills"));
 const Projects = lazy(() => import("./components/home/projects/Projects"));
 const Articles = lazy(() => import("./components/home/articles/Articles"));
-const PingPong = lazy(() => import("./components/home/games/PingPong"));
-const MemoryGame = lazy(() => import("./components/home/games/MemoryGame"));
+
 const WorkExperience = lazy(
   () => import("./components/home/work/WorkExperience")
 );
@@ -25,15 +24,6 @@ function App() {
             <PersonalHeader />
           </section>
 
-          <section id="games" className="scroll-mt-16">
-            <Suspense
-              fallback={
-                <div className="h-96 animate-pulse bg-slate-800/20 rounded-lg" />
-              }
-            >
-              <PingPong />
-            </Suspense>
-          </section>
           <section id="skills" className="scroll-mt-16">
             <Suspense
               fallback={
@@ -41,15 +31,6 @@ function App() {
               }
             >
               <Skills />
-            </Suspense>
-          </section>
-          <section id="memory" className="scroll-mt-16">
-            <Suspense
-              fallback={
-                <div className="h-96 animate-pulse bg-slate-800/20 rounded-lg" />
-              }
-            >
-              <MemoryGame />
             </Suspense>
           </section>
           <section id="projects" className="scroll-mt-16">
