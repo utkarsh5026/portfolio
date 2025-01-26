72df7c6003e723bb7fcca65366bee9cc05e5429a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-26 07:14:04 +0530

    Refactor SkillCardMoving component: Move skills array outside of component

--- app/src/components/home/skills/SkillCardMoving.tsx
+++ app/src/components/home/skills/SkillCardMoving.tsx
@@ -22,32 +22,33 @@ import {
   SiGo,
 } from "react-icons/si";
 
+const skills = [
+  {
+    icon: <SiJavascript className="text-yellow-500" />,
+    name: "JavaScript",
+  },
+  { icon: <SiDocker className="text-blue-500" />, name: "Docker" },
+  { icon: <SiReact className="text-blue-500" />, name: "React" },
+  { icon: <SiTypescript className="text-blue-500" />, name: "TypeScript" },
+  { icon: <SiNodedotjs className="text-green-500" />, name: "Node.js" },
+  { icon: <SiPython className="text-yellow-500" />, name: "Python" },
+  { icon: <SiGo className="text-blue-500" />, name: "Golang" },
+  { icon: <SiKubernetes className="text-blue-500" />, name: "Kubernetes" },
+  { icon: <SiGit className="text-red-800" />, name: "Git" },
+  { name: "MongoDB", icon: <SiMongodb className="text-green-500" /> },
+  { name: "PostgreSQL", icon: <SiPostgresql className="text-blue-500" /> },
+  { name: "Redis", icon: <SiRedis className="text-red-700" /> },
+  { name: "AWS", icon: <SiAmazon className="text-orange-500" /> },
+  { name: "Django", icon: <SiDjango className="text-green-500" /> },
+  { name: "Firebase", icon: <SiFirebase className="text-yellow-500" /> },
+  { name: "Tailwind", icon: <SiTailwindcss className="text-blue-500" /> },
+  { name: "FastAPI", icon: <SiFastapi className="text-green-500" /> },
+  { name: "Express", icon: <SiExpress className="text-red-500" /> },
+  { name: "Langchain", icon: <SiLangchain className="text-red-500" /> },
+];
+
 const SkillCardMoving: React.FC = () => {
   const elementRef = useRef<HTMLDivElement>(null);
-  const skills = [
-    {
-      icon: <SiJavascript className="text-yellow-500" />,
-      name: "JavaScript",
-    },
-    { icon: <SiDocker className="text-blue-500" />, name: "Docker" },
-    { icon: <SiReact className="text-blue-500" />, name: "React" },
-    { icon: <SiTypescript className="text-blue-500" />, name: "TypeScript" },
-    { icon: <SiNodedotjs className="text-green-500" />, name: "Node.js" },
-    { icon: <SiPython className="text-yellow-500" />, name: "Python" },
-    { icon: <SiGo className="text-blue-500" />, name: "Golang" },
-    { icon: <SiKubernetes className="text-blue-500" />, name: "Kubernetes" },
-    { icon: <SiGit className="text-red-800" />, name: "Git" },
-    { name: "MongoDB", icon: <SiMongodb className="text-green-500" /> },
-    { name: "PostgreSQL", icon: <SiPostgresql className="text-blue-500" /> },
-    { name: "Redis", icon: <SiRedis className="text-red-700" /> },
-    { name: "AWS", icon: <SiAmazon className="text-orange-500" /> },
-    { name: "Django", icon: <SiDjango className="text-green-500" /> },
-    { name: "Firebase", icon: <SiFirebase className="text-yellow-500" /> },
-    { name: "Tailwind", icon: <SiTailwindcss className="text-blue-500" /> },
-    { name: "FastAPI", icon: <SiFastapi className="text-green-500" /> },
-    { name: "Express", icon: <SiExpress className="text-red-500" /> },
-    { name: "Langchain", icon: <SiLangchain className="text-red-500" /> },
-  ];
 
   useEffect(() => {
     const element = elementRef.current;
