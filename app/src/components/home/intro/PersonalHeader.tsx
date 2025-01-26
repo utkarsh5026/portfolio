e844e95c5cc424d616dcc45a1e36a2cea544b318
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-26 08:37:03 +0530

    Refactor PersonalHeader: Enhance layout spacing for improved visual hierarchy and mobile responsiveness

--- app/src/components/home/intro/PersonalHeader.tsx
+++ app/src/components/home/intro/PersonalHeader.tsx
@@ -23,7 +23,7 @@ const PersonalHeaderComponent: React.FC = () => {
 
       <div className="container relative mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 lg:gap-8">
-          <div className="w-full space-y-4 sm:space-y-5 flex flex-col justify-start items-start md:order-1">
+          <div className="w-full space-y-8 sm:space-y-5 flex flex-col justify-start items-start md:order-1">
             <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold order-1">
               Hello, I'm{" "}
               <span className="bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">
@@ -31,7 +31,7 @@ const PersonalHeaderComponent: React.FC = () => {
               </span>
             </h1>
 
-            <div className="w-full md:hidden order-2 flex justify-center">
+            <div className="w-full md:hidden order-2 flex justify-center mb-8">
               <ProfileAvatar />
             </div>
 
@@ -39,7 +39,7 @@ const PersonalHeaderComponent: React.FC = () => {
               <AnimatedText statements={statements} />
             </div>
 
-            <p className="text-slate-400 max-w-[600px] text-base sm:text-lg lg:text-xl order-4 px-2 sm:px-4">
+            <p className="text-slate-400 max-w-[600px] text-base sm:text-lg lg:text-xl order-4 px-2 sm:px-4 mt-4">
               I'm a software engineer with a passion for creating user-friendly
               and efficient web applications. I'm a quick learner and I'm always
               looking to expand my skills. I develop mostly full-stack web
