b7f8e3f584adb92f0c15c7f475bd6ce53a86233a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-04 02:48:57 +0530

    Refactor Articles component with memoization and enhanced Section styling

--- app/src/components/home/articles/Articles.tsx
+++ app/src/components/home/articles/Articles.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Section from "@/components/base/Section";
+import Section from "@/components/section/Section";
 import ArticleCard from "./ArticleCard";
 import { articles } from "./articlesdump";
 
-const Articles: React.FC = () => {
+const ArticlesComponent: React.FC = () => {
   return (
-    <Section id="articles" label="Articles">
+    <Section id="articles" label="Articles" glowAccent="pink" scanlines={true}>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8 bg-ctp-base">
         {articles.map((article) => (
           <div className="h-full" key={article.title}>
@@ -17,4 +17,5 @@ const Articles: React.FC = () => {
   );
 };
 
+const Articles = React.memo(ArticlesComponent);
 export default Articles;
