0101a748649be8937d4d7cd4edff55d8ada42b0a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-05 10:43:52 +0530

    Refactor SideBar with reusable SideBarButton component and add resume view button

--- app/src/components/home/editor/SideBar.tsx
+++ app/src/components/home/editor/SideBar.tsx
@@ -6,10 +6,45 @@ import {
   FaGithub,
   FaTerminal,
   FaLinkedin,
+  FaEye,
 } from "react-icons/fa";
 import { BsLayoutSidebarInset } from "react-icons/bs";
 import { cn } from "@/lib/utils";
 import { useEditorContext } from "./context/explorerContext";
+
+// Sidebar Button component
+interface SideBarButtonProps {
+  icon: React.ReactNode;
+  label: string;
+  onClick: () => void;
+  isActive?: boolean;
+  className?: string;
+}
+
+const SideBarButton: React.FC<SideBarButtonProps> = ({
+  icon,
+  label,
+  onClick,
+  isActive = false,
+  className = "",
+}) => {
+  return (
+    <Button
+      variant="ghost"
+      className={cn(
+        "p-3 relative group transition-colors duration-200",
+        isActive ? "text-[#f38ba8]" : "text-[#cdd6f4] hover:text-[#89b4fa]",
+        className
+      )}
+      onClick={onClick}
+    >
+      {icon}
+      <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
+        {label}
+      </span>
+    </Button>
+  );
+};
 
 const SideBar: React.FC = () => {
   const { mobileMenuOpen, setActiveSection, explorerOpen, setExplorerOpen } =
@@ -23,87 +58,63 @@ const SideBar: React.FC = () => {
         mobileMenuOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
       )}
     >
-      <Button
-        variant="ghost"
-        className="p-3 text-[#cdd6f4] hover:text-[#89b4fa] mb-6 relative group"
+      <SideBarButton
+        icon={<FaCode className="w-5 h-5 text-ctp-green" />}
+        label="Home"
         onClick={() => setActiveSection("home")}
-      >
-        <FaCode className="w-5 h-5" />
-        <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-          Home
-        </span>
-      </Button>
+        className="mb-6"
+      />
 
-      <Button
-        variant="ghost"
-        className={cn(
-          "p-3 relative group transition-colors duration-200",
-          explorerOpen
-            ? "text-[#f38ba8]"
-            : "text-[#cdd6f4] hover:text-[#89b4fa]"
-        )}
+      <SideBarButton
+        icon={<BsLayoutSidebarInset className="w-5 h-5" />}
+        label={explorerOpen ? "Hide Explorer" : "Show Explorer"}
         onClick={() => setExplorerOpen(!explorerOpen)}
-      >
-        <BsLayoutSidebarInset className="w-5 h-5" />
-        <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-          {explorerOpen ? "Hide Explorer" : "Show Explorer"}
-        </span>
-      </Button>
+        isActive={explorerOpen}
+      />
 
-      <Button
-        variant="ghost"
-        className="p-3 text-[#cdd6f4] hover:text-[#89b4fa] relative group"
+      <SideBarButton
+        icon={<FaFolder className="w-5 h-5" />}
+        label="Projects"
         onClick={() => setActiveSection("projects")}
-      >
-        <FaFolder className="w-5 h-5" />
-        <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-          Projects
-        </span>
-      </Button>
+      />
 
-      <Button
-        className="p-3 text-ctp-lavender hover:text-ctp-blue relative group"
+      <SideBarButton
+        icon={<FaGithub className="w-4 h-4" />}
+        label="GitHub"
         onClick={() => window.open("https://github.com/utkarsh5026", "_blank")}
-      >
-        <FaGithub className="w-4 h-4" />
-        <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-          GitHub
-        </span>
-      </Button>
+        className="text-ctp-lavender hover:text-ctp-blue"
+      />
 
-      <Button
-        variant="ghost"
-        className="p-3 text-ctp-sapphire hover:text-ctp-blue relative group"
+      <SideBarButton
+        icon={<FaLinkedin className="w-4 h-4" />}
+        label="LinkedIn"
         onClick={() =>
           window.open(
             "https://www.linkedin.com/in/utkarsh-priyadarshi-8b5a731b9/",
             "_blank"
           )
         }
-      >
-        <FaLinkedin className="w-4 h-4" />
-        <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-          LinkedIn
-        </span>
-      </Button>
+        className="text-ctp-sapphire hover:text-ctp-blue"
+      />
+
+      <SideBarButton
+        icon={<FaEye className="w-5 h-5 text-ctp-red" />}
+        label="View Resume"
+        onClick={() =>
+          window.open(
+            "https://drive.google.com/file/d/1Rrg_GQMiwn2FL58L93sztTihyffN2sq9/view?usp=sharing"
+          )
+        }
+      />
 
       <div className="flex-1"></div>
 
-      <Button
-        variant="ghost"
-        className={cn(
-          "p-3 relative group transition-colors duration-200",
-          terminalOpen
-            ? "text-[#f38ba8]"
-            : "text-[#cdd6f4] hover:text-[#89b4fa]"
-        )}
+      <SideBarButton
+        icon={<FaTerminal className="w-5 h-5 text-ctp-peach" />}
+        label="Terminal"
         onClick={() => setTerminalOpen(!terminalOpen)}
-      >
-        <FaTerminal className="w-5 h-5" />
-        <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-          Terminal
-        </span>
-      </Button>
+        isActive={terminalOpen}
+      />
     </div>
   );
 };
