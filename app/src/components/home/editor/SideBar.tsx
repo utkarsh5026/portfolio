e339eca4993d8c902ebb800b71fbfd8556267ff3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-11 18:42:02 +0530

    Refactor SideBar component with GhostButton utility component

--- app/src/components/home/editor/SideBar.tsx
+++ app/src/components/home/editor/SideBar.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
 import {
   FaCode,
   FaFolder,
@@ -12,40 +11,7 @@ import {
 import { BsLayoutSidebarInset } from "react-icons/bs";
 import { cn } from "@/lib/utils";
 import { useEditorContext } from "./context/explorerContext";
-
-// Sidebar Button component
-interface SideBarButtonProps {
-  icon: React.ReactNode;
-  label: string;
-  onClick: () => void;
-  isActive?: boolean;
-  className?: string;
-}
-
-const SideBarButton: React.FC<SideBarButtonProps> = ({
-  icon,
-  label,
-  onClick,
-  isActive = false,
-  className = "",
-}) => {
-  return (
-    <Button
-      variant="ghost"
-      className={cn(
-        "p-3 relative group transition-colors duration-200",
-        isActive ? "text-[#f38ba8]" : "text-[#cdd6f4] hover:text-[#89b4fa]",
-        className
-      )}
-      onClick={onClick}
-    >
-      {icon}
-      <span className="absolute left-14 px-2 py-1 bg-[#313244] rounded text-xs invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap">
-        {label}
-      </span>
-    </Button>
-  );
-};
+import GhostButton from "@/components/utils/GhostButton";
 
 const SideBar: React.FC = () => {
   const { mobileMenuOpen, setActiveSection, explorerOpen, setExplorerOpen } =
@@ -59,34 +25,34 @@ const SideBar: React.FC = () => {
         mobileMenuOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
       )}
     >
-      <SideBarButton
+      <GhostButton
         icon={<FaCode className="w-5 h-5 text-ctp-green" />}
         label="Home"
         onClick={() => setActiveSection("home")}
         className="mb-6"
       />
 
-      <SideBarButton
+      <GhostButton
         icon={<BsLayoutSidebarInset className="w-5 h-5" />}
         label={explorerOpen ? "Hide Explorer" : "Show Explorer"}
         onClick={() => setExplorerOpen(!explorerOpen)}
         isActive={explorerOpen}
       />
 
-      <SideBarButton
+      <GhostButton
         icon={<FaFolder className="w-5 h-5" />}
         label="Projects"
         onClick={() => setActiveSection("projects")}
       />
 
-      <SideBarButton
+      <GhostButton
         icon={<FaGithub className="w-4 h-4" />}
         label="GitHub"
         onClick={() => window.open("https://github.com/utkarsh5026", "_blank")}
         className="text-ctp-lavender hover:text-ctp-blue"
       />
 
-      <SideBarButton
+      <GhostButton
         icon={<FaLinkedin className="w-4 h-4" />}
         label="LinkedIn"
         onClick={() =>
@@ -98,7 +64,7 @@ const SideBar: React.FC = () => {
         className="text-ctp-sapphire hover:text-ctp-blue"
       />
 
-      <SideBarButton
+      <GhostButton
         icon={<FaEye className="w-5 h-5 text-ctp-red" />}
         label="View Resume"
         onClick={() =>
@@ -110,13 +76,13 @@ const SideBar: React.FC = () => {
 
       <div className="flex-1"></div>
 
-      <SideBarButton
+      <GhostButton
         icon={<FaFolderOpen className="w-5 h-5 text-ctp-mauve" />}
         label="Github Repo for Portfolio"
         onClick={() => window.open("https://github.com/utkarsh5026/Portfolio")}
       />
 
-      <SideBarButton
+      <GhostButton
         icon={<FaTerminal className="w-5 h-5 text-ctp-peach" />}
         label="Terminal"
         onClick={() => setTerminalOpen(!terminalOpen)}
