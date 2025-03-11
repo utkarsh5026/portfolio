8351d4cbeb232d9d54c3bdaeea50ac2353b76245
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-03-11 18:12:59 +0530

    Add TourPortal to MainPortfolio with TourProvider wrapper

--- app/src/components/load/MainPortfolio.tsx
+++ app/src/components/load/MainPortfolio.tsx
@@ -1,11 +1,16 @@
 import { EditorProvider } from "@/components/home/editor/context/EditorProvider";
 import CodeEditor from "@/components/home/editor/CodeEditor";
+import TourPortal from "../home/tour/TourPortal";
+import { TourProvider } from "../home/tour/context/TourProvider";
 
 const MainPortfolio = () => {
   return (
-    <EditorProvider>
-      <CodeEditor />
-    </EditorProvider>
+    <TourProvider>
+      <EditorProvider>
+        <CodeEditor />
+        <TourPortal />
+      </EditorProvider>
+    </TourProvider>
   );
 };
 
