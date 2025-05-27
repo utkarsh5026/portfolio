6f1cb4d88209f45be30219e8cf9b4f8b52dce184
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-05-27 12:30:15 +0530

    Add utility index file to export common components
    This commit introduces a new index file in the utils directory, exporting the ProgressBar, Cursor, DialogModal, and GhostButton components. This change enhances modularity and simplifies imports across the application, improving overall code organization and maintainability.

---
+++ app/src/components/utils/index.ts
@@ -0,0 +1,6 @@
+import ProgressBar from "./ProgressBar";
+import Cursor from "./Cursor";
+import DialogModal from "./DialogModal";
+import GhostButton from "./GhostButton";
+
+export { ProgressBar, Cursor, DialogModal, GhostButton };
