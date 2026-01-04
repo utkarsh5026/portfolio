3a1a530d6b5eab44d9095d4389ed2fd008967e63
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-01-04 20:39:22 +0530

    refactor: remove tour-related components and styles
    - Deleted TourTooltip component and its associated styles.
    - Removed TourButton, TourFloatingButton, TourLoading, TourProgress, TourStepFinalMessage, and TypeWriter components.
    - Cleaned up TourProvider and TourPortal usage in MainPortfolio component.

--- app/src/components/load/main-portfolio.tsx
+++ app/src/components/load/main-portfolio.tsx
@@ -1,15 +1,10 @@
 import { EditorProvider } from "@/components/home/editor/context/editor-provider";
 import CodeEditor from "@/components/home/editor/CodeEditor";
-import TourPortal from "../home/tour/TourPortal";
-import { TourProvider } from "../home/tour/context/TourProvider";
 
 const MainPortfolio = () => {
   return (
     <EditorProvider>
-      <TourProvider>
-        <CodeEditor />
-        <TourPortal />
-      </TourProvider>
+      <CodeEditor />
     </EditorProvider>
   );
 };
