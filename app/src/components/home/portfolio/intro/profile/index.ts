8693699ae0f093dd21c7757e65cc3e3b4f12a791
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-03-05 03:20:33 +0530

    feat: add MiniProjects component to profile intro section

--- app/src/components/home/portfolio/intro/profile/index.ts
+++ app/src/components/home/portfolio/intro/profile/index.ts
@@ -2,5 +2,12 @@ import PersonalTitle from "./personal-title";
 import PersonalDescription from "./personal-description";
 import ProfilePicture from "./profile-picture";
 import ProfileButtons from "./profile-buttons";
+import MiniProjects from "./mini-projects";
 
-export { PersonalTitle, PersonalDescription, ProfilePicture, ProfileButtons };
+export {
+  PersonalTitle,
+  PersonalDescription,
+  ProfilePicture,
+  ProfileButtons,
+  MiniProjects,
+};
